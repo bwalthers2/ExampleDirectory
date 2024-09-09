dcb854c1bc91afd159d4f907b38b8a6338479f07
Bjorn Walthers <bwalthers2@hwemail.com>
2024-09-08 19:17:31 -0700

    Add hidden and non-hidden files using chosen writing method; create .gitignore file

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -5,47 +5,26 @@ import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter {
     public static void main(String[] args) {
-        String data = "Hello, World!";
+        String passwordString = "This Is Top Secret! Get Out Of Here!";
         String fileName1 = "example1.txt";
         String fileName2 = "example2.txt";
         String fileName3 = "example3.txt";
         String fileName4 = "example4.txt";
         String fileName5 = "example5.txt";
-
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String fileInHiddenDir = "fileInHiddenDir.txt";
+        String dataInHiddenDir = "This is in a hidden directory. Private stuff .....";
+        String passwordFile = ".passwordFile.txt";
 
         // 5. Using Files (java.nio.file)
         try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get(passwordFile), passwordString.getBytes(StandardCharsets.UTF_8));
+            File hiddenDir = new File(".secretDirectory");
+            hiddenDir.mkdir();
+            Path hiddenDirPath = Paths.get(".secretDirectory");
+            Path filePathHidden = hiddenDirPath.resolve(fileInHiddenDir);
+            Files.write(filePathHidden, dataInHiddenDir.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("This is extra commit code");
     }
 }
